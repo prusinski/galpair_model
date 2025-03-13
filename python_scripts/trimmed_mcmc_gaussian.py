b5e151e1ea2ad46b992c26158f0439784cc25315
Nikolaus Prusinski <nik@astro.caltech.edu>
2025-03-12 19:59:04 -0700

    250312

--- python_scripts/trimmed_mcmc_gaussian.py
+++ python_scripts/trimmed_mcmc_gaussian.py
@@ -1,4 +1,3 @@
-%matplotlib widget
 import numpy as np
 from astropy import units as u, constants as c
 import matplotlib.pyplot as plt
@@ -8,18 +7,17 @@ from astropy.convolution import convolve, Gaussian2DKernel
 from scipy.optimize import fsolve, fmin
 from astropy.io import fits
 import scipy.interpolate as interp
-
+import emcee
+import corner
+from multiprocessing import Pool
+import multiprocessing as mp
 
 cnfw = 3.3 # NFW concentration parameter
 z = 2.3 # redshift
 Rs = 27 # kpc = Rvir = 90 kpc
 H = cosmo.H(z).to('km/s/kpc').value
 
-noise_level = 0.05
-# print(H)
-# M_h = 1e12*u.M_sun
-# rho0 = 3*M_h/(4*np.pi*Rs**3) * 0.5
-# A = 8*np.pi*c.G*rho0*Rs**3/(1*u.kpc)
+noise_level = 0.02
 A = 1.2e7 #* u.km**2/u.s**2
 
 bres = 5 # kpc
@@ -33,16 +31,18 @@ bm, vm = np.meshgrid(bvec, vrawsamp)
 bvec_final = np.arange(min_b, max_b+2.5, 5)
 vvec_final = np.arange(-1000, 1025, 50)
 
+lya_real = fits.open('../lya_conv_250312.fits')[0].data
+lya_err_real = fits.open('../lya_error_conv_250311.fits')[0].data
 
-def vins(r, vs, sigma, tau):
-    # vin = voff # + H*r
-    vin = tau * np.exp(-(vs)**2/(2*sigma**2))
-    return vin
+# def vins(r, vs, sigma, tau):
+#     # vin = voff # + H*r
+#     vin = tau * np.exp(-(vs)**2/(2*sigma**2))
+#     return vin
 
 def vouts(r, vi):
     vout = np.sqrt(vi**2 + A*(-np.log((Rs+1)/Rs) + (1/r * np.log((Rs+r)/Rs))))
     return vout
-    
+
 def ains(r, a0in, gin):
     ain = (a0in * (r/100)**(-gin))
     return ain
@@ -50,6 +50,7 @@ def ains(r, a0in, gin):
 def aouts(r, a0out, gout):
     aout = (a0out * (r/100)**(-gout))
     return aout
+
 
 
 def model(theta):
@@ -62,15 +63,15 @@ def model(theta):
     # rmax_out = np.min([rmax_out_est, rmax])
     # print(rmax_out)
     # rmax_in_est = 100 * (a0in/noise_level)**(1/gin)
-    
+
     # rmax_in = np.min([rmax_in_est, rmax])
     # print(rmax_in)
 
 
     rawhmap = np.zeros((len(vrawsamp), len(bvec)))
 
-    
-    
+
+
     for bi, b in enumerate(bvec):
 
         lmax_out = np.sqrt(rmax_out**2 - b**2)
@@ -84,13 +85,13 @@ def model(theta):
         # larr_out = np.concatenate([larr_slowl_out, larr_fast_out, larr_slowr_out])
 
         larr_out = np.linspace(-lmax_out, lmax_out, 2000)
-        
+
         r_out = np.sqrt(larr_out**2 + b**2)
-        
+
         tau_outs = aouts(r_out, a0out, gout)
         tau_outs_arr = np.array(tau_outs)
         # print(tau_outs_arr)
-        
+
 
         # larr_slowl_in = np.linspace(-lmax_in, -100, 50)
         # nfast_in = int(500 * np.exp(-b/100)) #1500
@@ -100,10 +101,10 @@ def model(theta):
 
         larr_in = np.linspace(-lmax_in, lmax_in, 68)
         r_in = np.sqrt(larr_in**2 + b**2)
-        
+
         # lmaxcalc = np.sqrt((-voff/H)**2-b**2)
         # print(lmaxcalc)
-        
+
         tau_ins = ains(r_in, a0in, gin)
         # tau_ins_arr = np.array(tau_ins)
 
@@ -111,19 +112,10 @@ def model(theta):
         # plt.plot(larr_out, tau_outs, 'b.')
         # # plt.plot(larr_in, tau_ins, 'r.')
         # plt.show()
-        
+
 
         vLOS_out = larr_out/r_out*vouts(r_out, vi)
-
-        vsamp = np.linspace(-1000, 1000, 2000)
-        
-        # vLOS_in = vins(r_in, vsamp, voff, a0in) #larr_in/r_in*
-        
-        # print(vLOS_in)
-
-        # plt.figure()
-        # plt.plot(larr_in, vLOS_in, c=tau_ins, cmap='inf)
-        
+        # vLOS_in = larr_in/r_in*vins(r_in, voff)
         # plt.figure()
         # sc = plt.scatter(larr_out, vLOS_out, c=tau_outs)
         # sc1 = plt.scatter(larr_in, vLOS_in, c=tau_ins, cmap='inferno')
@@ -136,33 +128,32 @@ def model(theta):
         # plt.axvline(0, c = 'k', alpha = 0.7, lw = 0.7, ls = '--')
         # plt.tight_layout()
         # plt.show()
-    
+
         maxvout = np.nanmax(vLOS_out)
         # maxvin = np.nanmax(vLOS_in)
 
-        taulist_out = []
-        # taulist_in = []
-        
         # bad b's
         if ~np.isfinite(maxvout):
             for j in vrawsamp:
                 taulist_out.append([j, 0])
             continue
-            
+
         # if ~np.isfinite(maxvin):
         #     # print(f'bad {b}')
         #     for j in vrawsamp:
         #         taulist_in.append([j, 0])
         #     continue
-        
+
         l_minvout = larr_out[np.argmin(vLOS_out)]
         l_maxvout = larr_out[np.argmax(vLOS_out)]
 
         # l_minvin = larr_in[np.argmin(vLOS_in)]
         # l_maxvin = larr_in[np.argmax(vLOS_in)]
 
-        
-        
+
+        taulist_out = []
+        # taulist_in = []
+
         for vl in vrawsamp:
             # outflow
             if np.abs(vl) > maxvout:
@@ -171,19 +162,19 @@ def model(theta):
                 possible_values = larr_out[(vLOS_out > vl) & (vLOS_out < vl+30)]
                 l_near = possible_values[(possible_values > l_minvout) & (possible_values < l_maxvout)]
                 l_far = possible_values[(possible_values < l_minvout) | (possible_values > l_maxvout)]
-        
+
                 tau_near_inds = [np.argwhere(larr_out == ln)[0][0] for ln in l_near]
                 tau_out_near = tau_outs_arr[tau_near_inds]
-        
-                
+
+
                 tau_far_inds = [np.argwhere(larr_out == ln)[0][0] for ln in l_far]
                 tau_out_far = tau_outs_arr[tau_far_inds]
                 tau_tot_out = np.nan_to_num(np.nanmean(tau_out_near)) + np.nan_to_num(np.nanmean(tau_out_far))
-    
+
                 if np.isfinite(tau_tot_out):
                     taulist_out.append([vl+15, tau_tot_out])
 
-            # inflow
+            # # inflow
             # if np.abs(vl) > maxvin:
             #     taulist_in.append([vl+15, 0])
             # else:
@@ -191,26 +182,25 @@ def model(theta):
             #     # print(possible_values)
             #     l_near = possible_values[(possible_values < l_minvin) & (possible_values > l_maxvin)]
             #     l_far = possible_values[(possible_values > l_minvin) | (possible_values < l_maxvin)]
-        
+            #
             #     tau_near_inds = [np.argwhere(larr_in == ln)[0][0] for ln in l_near]
             #     tau_in_near = tau_ins_arr[tau_near_inds]
-        
-                
+            #
+            #
             #     tau_far_inds = [np.argwhere(larr_in == ln)[0][0] for ln in l_far]
             #     tau_in_far = tau_ins_arr[tau_far_inds]
             #     tau_tot_in = np.nan_to_num(np.nanmean(tau_in_near)) + np.nan_to_num(np.nanmean(tau_in_far))
             #     # print(tau_tot)
-    
+            #
             #     if np.isfinite(tau_tot_in):
             #         taulist_in.append([vl+15, tau_tot_in])
-        
+
         tauarr_out_wzeros = np.array(taulist_out) # v, tau
         # tauarr_in_wzeros = np.array(taulist_in)
 
         tauarr_out = tauarr_out_wzeros[tauarr_out_wzeros[:,1] > 0]
         # tauarr_in = tauarr_in_wzeros[tauarr_in_wzeros[:,1] > 0]
-        
-        
+
         if len(tauarr_out) == 0:
             tout = vrawsamp*0.0
         else:
@@ -220,15 +210,14 @@ def model(theta):
         #     tin = vrawsamp*0.0
         # else:
         #     tin = np.interp(vrawsamp, tauarr_in[:,0], tauarr_in[:,1], left=0, right=0)
-
         tin = tau_ins * np.exp( - (vrawsamp - 0)**2 / (2 * voff**2))
 
         rawhmap[:,bi] = tout + tin
             # tau_tot = tau_tot_out + tau_tot_in
             # taulist.append([b, vl+15, tau_tot])
 
-    
-    
+
+
 
     # points = tauarr[:,0:2]
     # values = tauarr[:,2]
@@ -239,45 +228,45 @@ def model(theta):
     # plt.show()
 
 
-    
-    
+
+
     # print(tauarr_out[np.isfinite(tauarr_out[:,2])])
-        # plt.figure()
-        # plt.scatter(tauarr_out[:,0], tauarr_out[:,1])
-        # plt.plot(vrawsamp, tout)
-        # plt.plot(vrawsamp, tin)
-        # plt.show()
-    
+    # plt.figure()
+    # plt.scatter(tauarr_out[:,1], tauarr_out[:,2])
+    # plt.plot(vrawsamp, tout)
+    # plt.plot(vrawsamp, tin)
+    # plt.show()
+
     # plt.figure()
     # sc = plt.scatter(tauarr_out[:,0], tauarr_out[:,1], c=tauarr_out[:,2]) #/cmap='inferno_r' vmin=2e-2, vmax = 1e-1 norm=matplotlib.colors.LogNorm()
     # plt.colorbar(sc, label = '$\\tau_\\mathrm{out}$')
-    
+
     # # tin = plt.scatter(tauarr_in[:,0], tauarr_in[:,1], c=tauarr_in[:,2], cmap='plasma_r') #/cmap='inferno_r' vmin=2e-2, vmax = 1e-1 norm=matplotlib.colors.LogNorm()
     # # plt.colorbar(tin, label = '$\\tau_\\mathrm{in}$')
     # plt.xlabel('b (kpc)')
     # plt.ylabel('$v_\\mathrm{LOS}$ (km/s)')
     # plt.tight_layout()
     # plt.show()
-    
+
     # make map
-    
+
     # vres = 10 #50
     # vlosr = np.arange(-1000, 1000, vres)
-    
+
     # hmap = np.zeros((len(vlosr), len(bvec)))
-    
+
     # for bi, b in enumerate(bvec):
     #     bcut_out = (tauarr_out[:,0] == b)
     #     bcut_in = (tauarr_in[:,0] == b)
-    
+
     #     for vind, v in enumerate(vlosr):
     #         vcut_out = (tauarr_out[:,1] > v) & (tauarr_out[:,1] < v+vres)
     #         vcut_in = (tauarr_in[:,1] > v) & (tauarr_in[:,1] < v+vres)
-            
+
     #         tauavg_out = np.nanmean(tauarr_out[:,2][bcut_out & vcut_out])
     #         if ~np.isfinite(tauavg_out):
     #             tauavg_out = 0
-    
+
     #         tauavg_in = np.nanmean(tauarr_in[:,2][bcut_in & vcut_in])
     #         if ~np.isfinite(tauavg_in):
     #             tauavg_in = 0
@@ -288,16 +277,16 @@ def model(theta):
 
     f = interp.RectBivariateSpline(vrawsamp, bvec, hmap_conv, kx=3, ky=3)
     hmap_reshaped = f(vvec_final, bvec_final)
-    
+
     # print(new_im.shape)
-    
+
     # plt.figure()
     # plt.imshow(hmap_reshaped, aspect='auto', origin='lower', extent = [bvec_final.min(), bvec_final.max(), vvec_final.min(), vvec_final.max()])
     # plt.show()
-    
+
     # # # print(hmap.shape)
     # plt.figure()
-    # us = plt.imshow(rawhmap, origin='lower', extent = (bvec[0], bvec[-1], vrawsamp[0], vrawsamp[-1]), 
+    # us = plt.imshow(rawhmap, origin='lower', extent = (bvec[0], bvec[-1], vrawsamp[0], vrawsamp[-1]),
     #            aspect='auto', cmap='plasma')
     # plt.colorbar(us, label = '$\\tau_\\mathrm{tot}$')
     # plt.xlabel('b (kpc)')
@@ -305,9 +294,9 @@ def model(theta):
     # plt.title('Unsmoothed')
     # # plt.xscale('log')
     # plt.show()
-    
+
     # plt.figure()
-    # sm = plt.imshow(hmap_conv, origin='lower', extent = (bvec[0], bvec[-1], vrawsamp[0], vrawsamp[-1]), 
+    # sm = plt.imshow(hmap_conv, origin='lower', extent = (bvec[0], bvec[-1], vrawsamp[0], vrawsamp[-1]),
     #            aspect='auto', cmap='inferno')
     # plt.colorbar(sm, label = '$\\tau_\\mathrm{tot}$')
     # plt.xlabel('b (kpc)')
@@ -317,74 +306,55 @@ def model(theta):
     # plt.show()
 
     return hmap_reshaped
-    
-
-
-
-lya_real = fits.open('lya_conv_250312.fits')[0].data
-# binterp_vec = np.arange(10,252.5,5)
-# vinterp_vec = np.arange(-1000, 1050, 100)
-
-
-def lnlike(theta, truth = lya_real):
-    lya_model = model(theta)
-    return -0.5 * np.nansum((lya_real - lya_model)**2), lya_model
-
-
-rr = np.linspace(10,250, 1000)*u.kpc
-rvir = 90*u.kpc
-vvir = np.sqrt(c.G*1e12*u.M_sun/rvir)
-x = rr/rvir
-
-def f(x):
-    return np.log(1+x) + x/(1+x)
-
-vcirc = vvir * np.sqrt(f(cnfw*x)/(x*f(cnfw)))
-vcirc = np.sqrt(c.G*1e12*u.M_sun/rr)
-
-vcirc = np.sqrt(vvir**2/x*(np.log(1+cnfw*x)-(cnfw*x)/(1+cnfw*x))/(np.log(1+cnfw)-cnfw/(1+cnfw)))
-
-vesc = np.sqrt(2*c.G*1e12*u.M_sun/rr*np.log(1+cnfw*rr/rvir)/(np.log(1+cnfw)-cnfw/(1+cnfw)))
-
-
-
-# theta = 905, -450, 0.13, 0.22, 0.45, 0.4 #vout, vin, aout, ain, gout, gin
-theta = 750, 258, 0.2, 0.2, 0.4, 0.33
-likelihood, lya_model = lnlike(theta)
-print(likelihood)
-vmi, vma = -0, 0.5
-
-fig, ax = plt.subplots(1,3, figsize = (14,6), sharex=True, sharey=True)
-re = ax[0].imshow(lya_real, aspect='auto', origin='lower', 
-                  extent = [bvec_final.min(), bvec_final.max(), vvec_final.min(), vvec_final.max()], vmin = vmi, vmax = vma)
-fig.colorbar(re, ax=ax[0])
-ax[0].set_title('Lya Real')
-
-mod = ax[1].imshow(lya_model, aspect='auto', origin='lower', 
-                   extent = [bvec_final.min(), bvec_final.max(), vvec_final.min(), vvec_final.max()], vmin = vmi, vmax = vma)
-fig.colorbar(mod, ax=ax[1])
-ax[1].set_title('Lya Model')
-
-diff = ax[2].imshow(lya_real - lya_model, aspect='auto', origin='lower',
-                    extent = [bvec_final.min(), bvec_final.max(), vvec_final.min(), vvec_final.max()], cmap='bwr', vmin=-0.3, vmax=0.3)
-fig.colorbar(diff, ax=ax[2])
-ax[2].set_title('Lya Real - Lya Model')
-fig.tight_layout()
-plt.show()
 
 
 
 
+def lnlike(theta):
+    # return -0.5 * np.nansum(((lya_real - model(theta))/lya_err_real)**2)
+    return -0.5 * np.nansum((lya_real - model(theta))**2)
 
-plt.figure()
-plt.step(vvec_final, -np.exp(lya_model[:,0]))
-plt.show()
+def lnprior(theta):
+    vi, voff, a0out, a0in, gout, gin = theta
 
+    vir = (500 < vi < 900)
+    voffr = (0 < voff < 400)
+    a0outr = (0 < a0out < 0.4)
+    a0inr = (0. < a0in < 0.4)
+    goutr = (0 < gout < 1.5)
+    ginr = (0. < gin < 1.5)
 
+    if vir and voffr and a0outr and a0inr and goutr and ginr:
+        return 0.0
 
+    return -np.inf
 
+def lnprob(theta):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlike(theta)
 
+if __name__ == '__main__':
 
+    nwalkers = 60
+    niter = 7500
+    initial = np.array([750, 258, 0.2, 0.2, 0.45, 0.31])
+    ndims = len(initial)
 
+    p0 = [initial * (1 + 0.01*np.random.randn(ndims)) for i in range(nwalkers)]
+    # print(p0)
 
+    filename = "../MCMC_outputs/trimmed_60w_7500it_250312-both.h5"
+    backend = emcee.backends.HDFBackend(filename)
+    backend.reset(nwalkers, ndims)
 
+    with Pool(10) as pool:
+
+        sampler = emcee.EnsembleSampler(nwalkers, ndims, lnprob, pool=pool, backend=backend)
+        sampler.reset()
+
+        print("Running production...")
+        pos, prob, state = sampler.run_mcmc(p0, niter, progress=True)
+
+        # return sampler, pos, prob, state
